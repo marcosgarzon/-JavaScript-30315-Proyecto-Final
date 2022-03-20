9eb131223a4fa63411825c9c6e43cb04e3e7cb86
Marcos Garzon <marcosgarzonmdq@gmail.com>
2022-03-20 17:59:43 -0300

    test 1

--- js/script.js
+++ js/script.js
@@ -1,26 +1,23 @@
-// Ejecutar: Armar lista
-let arrOpcion = [];
-let arr2 = [];
 
 (() => {
   let tiempo = new Date();
   let semana = tiempo.getDay();
-  if ((semana = 0)) {
+  if ((semana == 0)) {
     document.getElementById("ofert").innerHTML =
       "<img src='./assets/domingo.png'>";
-  } else if ((semana = 1)) {
+  } else if ((semana == 1)) {
     document.getElementById("ofert").innerHTML =
       "<img src='./assets/lunes.png'>";
-  } else if ((semana = 2)) {
+  } else if ((semana == 2)) {
     document.getElementById("ofert").innerHTML =
       "<img src='./assets/martes.png'>";
-  } else if ((semana = 3)) {
+  } else if ((semana == 3)) {
     document.getElementById("ofert").innerHTML =
       "<img src='./assets/miercoles.png'>";
-  } else if ((semana = 4)) {
+  } else if ((semana == 4)) {
     document.getElementById("ofert").innerHTML =
       "<img src='./assets/jueves.png'>";
-  } else if ((semana = 5)) {
+  } else if ((semana == 5)) {
     document.getElementById("ofert").innerHTML =
       "<img src='./assets/viernes.png'>";
   } else {
@@ -31,57 +28,72 @@ let arr2 = [];
 
 // Autoejecutable, foto que cambia según el día de la semana
 
-let ListaDeCompras = () => {
-  alert("Vamos a realizar una lista de compras");
-  let term = "n";
-  let agregar = null;
-  while (term == "n") {
-    agregar = prompt("Ingrese un producto");
-    if (agregar != null) {
-      arrOpcion.push(agregar);
-    } else {
-      alert("No siguió la consigna");
-    }
-    let contin = confirm("¿Desea agregar otro producto?");
-    if (!contin) {
-      term = "y";
-      alert("La lista de compras es la siguiente: " + arrOpcion.join(",  "));
-    }
-  }
-  document.getElementById(
-    "lis"
-  ).innerHTML = `Lista de compras: <li><p class="animacion1"> ${arrOpcion.join(
-    '<li><p class="animacion1">'
-  )}`;
-  return arrOpcion;
+
+// A PARTIR DE ACA ES TODO TESTING
+
+
+const elementosAlmacenados = localStorage.getItem("elementos");
+let elementos = [];
+
+if (elementosAlmacenados) {
+  elementos = JSON.parse(elementosAlmacenados);
 };
 
-// Ejecutar: Ingresar precios
-let impList = () => {
-  class Precio {
-    constructor(producto, valor) {
-      this.producto = producto;
-      this.valor = parseFloat(valor);
-    }
-  }
+mostrarElementos();
 
-  for (const arrOpcions of arrOpcion) {
-    let valor = prompt(`Costo de: ${arrOpcions}`);
-    arr2.push(new Precio(arrOpcions, valor));
-  }
-  let total = 0;
-  arr2.forEach((p) => {
-    total += p.valor;
-  });
-  alert(`Precio final: $ ${total}`);
-  document.getElementById("tot").innerHTML = `Precio final: $ ${total}`;
+function agregarItem () {
+  const inputRef = document.getElementById("input");
+  const valor = inputRef.value;
+
+  
+
+// validación
+
+if (valor.trim()){
+  elementos.push(valor);
+  inputRef.value = "";
+  mostrarElementos();
+  localStorage.setItem("elementos", JSON.stringify(elementos));
+}
+else {
+  alert("Ingresa un producto");
+};
 };
 
-let filtrarPres = () => {
-  let faltante = prompt(
-    "Ingrese un monto para filtrar los productos que sean iguales o superiores a ese valor"
-  );
-  let resultado = arr2.filter((el) => el.valor >= faltante);
+function mostrarElementos(){
+  const contenedor = document.getElementById("elementos");
+  contenedor.innerHTML = "";
 
-  alert(JSON.stringify(resultado));
+
+for (const element of elementos) {
+  const item = document.createElement("li");
+  item.className = "list-group-item";
+  item.addEventListener("click", (e) => {
+  e.target.remove();
+  const index = elementos.findIndex((el) => el === e.target.textContent);
+  elementos.splice(index, 1);
+  localStorage.setItem("elementos", JSON.stringify(elementos));
+  })
+  item.textContent = element;
+  contenedor.appendChild(item);
+}
+
+const active = document.getElementById("active");
+active.textContent = `Activos: ${elementos.length}`
+};
+
+// const agregarEnter = () => {
+//   const inputRef = document.getElementById("input");
+//   inputRef.keypress(function(event){
+//   if (event.which === 13) {
+//     agregarItem();
+//   };
+//   });
+// };
+
+
+function pulsar(e) {
+  if (e.keyCode === 13 && !e.shiftKey) {
+    agregarItem ();
+  }
 };
