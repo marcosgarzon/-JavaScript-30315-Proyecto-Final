7fd3e948509b05d065228e3e78ba8bcefd843559
Marcos Garzon <marcosgarzonmdq@gmail.com>
2022-03-22 20:20:41 -0300

    Optimización 1

--- js/script.js
+++ js/script.js
@@ -2,22 +2,22 @@
 (() => {
   let tiempo = new Date();
   let semana = tiempo.getDay();
-  if ((semana == 0)) {
+  if (semana == 0) {
     document.getElementById("ofert").innerHTML =
       "<img src='./assets/domingo.png'>";
-  } else if ((semana == 1)) {
+  } else if (semana == 1) {
     document.getElementById("ofert").innerHTML =
       "<img src='./assets/lunes.png'>";
-  } else if ((semana == 2)) {
+  } else if (semana == 2) {
     document.getElementById("ofert").innerHTML =
       "<img src='./assets/martes.png'>";
-  } else if ((semana == 3)) {
+  } else if (semana == 3) {
     document.getElementById("ofert").innerHTML =
       "<img src='./assets/miercoles.png'>";
-  } else if ((semana == 4)) {
+  } else if (semana == 4) {
     document.getElementById("ofert").innerHTML =
       "<img src='./assets/jueves.png'>";
-  } else if ((semana == 5)) {
+  } else if (semana == 5) {
     document.getElementById("ofert").innerHTML =
       "<img src='./assets/viernes.png'>";
   } else {
@@ -26,107 +26,91 @@
   }
 })();
 
-
 // DESDE ACA ESTA POR FUERA DE FUNCIONES
-
 
 const elementosAlmacenados = localStorage.getItem("elementos");
 
 let elementos = [];
 
-
 if (elementosAlmacenados) {
   elementos = JSON.parse(elementosAlmacenados);
-};
-
-
+}
 
 mostrarElementos();
 
 // HASTA ACA ESTA POR FUERA DE FUNCIONES
 
 // AGREGAR ITEM FUNCION
-function agregarItem () {
+function agregarItem() {
   const inputRef = document.getElementById("input");
   const valor = inputRef.value;
 
-  
+  // validación
 
-        // validación
-
-if (valor.trim()){
-  elementos.push(valor);
-  inputRef.value = "";
-  localStorage.setItem("elementos", JSON.stringify(elementos));
-  tablita();
-  mostrarElementos();
+  if (valor.trim()) {
+    elementos.push(valor);
+    inputRef.value = "";
+    localStorage.setItem("elementos", JSON.stringify(elementos));
+    tablita();
+    mostrarElementos();
+  } else {
+    alert("Ingresa un producto");
+  }
 }
-else {
-  alert("Ingresa un producto");
-};
-};
 
 // MOSTRAR ELEMENTOS FUNCION
 
-function mostrarElementos(){
+function mostrarElementos() {
   const contenedor = document.getElementById("elementos");
   contenedor.innerHTML = "";
 
+  for (const element of elementos) {
+    const item = document.createElement("li");
+    item.className = "item-lista text-center";
+    item.addEventListener("click", (e) => {
+      e.target.remove();
+      const index = elementos.findIndex((el) => el === e.target.textContent);
+      elementos.splice(index, 1);
+      localStorage.setItem("elementos", JSON.stringify(elementos));
+      tablita();
+      mostrarElementos();
+    });
+    item.textContent = element;
+    contenedor.appendChild(item);
+  }
 
-for (const element of elementos) {
-  const item = document.createElement("li");
-  item.className = "item-lista text-center";
-  item.addEventListener("click", (e) => {
-  e.target.remove();
-  const index = elementos.findIndex((el) => el === e.target.textContent);
-  elementos.splice(index, 1);
-  localStorage.setItem("elementos", JSON.stringify(elementos));
-  tablita();
-  mostrarElementos();
-  })
-  item.textContent = element;
-  contenedor.appendChild(item);
+  const active = document.getElementById("active");
+  active.textContent = `Activos: ${elementos.length}`;
 }
-
-const active = document.getElementById("active");
-active.textContent = `Activos: ${elementos.length}`
-};
 
 // INPUT CON ENTER DE TO DO LIST
 
-
 function pulsar(e) {
   if (e.keyCode === 13 && !e.shiftKey) {
-    agregarItem ();
+    agregarItem();
   }
-};
+}
 
 // IMPRIMIR TABLA DE COSTOS
 let tablita = () => {
-let table = document.getElementById("target");
-table.innerHTML = `<tr><th class="col-md-6">Producto</th><th class="col-md-6">Costo</th></tr>`;
-elementos.forEach((el, index)=>{
-  table.innerHTML += `<tr><td> ${el} </td><td class="text-end">$ <input type="number" class="nilai text-end" placeholder="Ingresar precio"></td></tr>`;
-})
+  let table = document.getElementById("target");
+  table.innerHTML = `<tr><th class="col-md-6">Producto</th><th class="col-md-6">Costo</th></tr>`;
+  elementos.forEach((el, index) => {
+    table.innerHTML += `<tr><td> ${el} </td><td class="text-end">$ <input type="number" class="nilai text-end" placeholder="Ingresar precio"></td></tr>`;
+  });
 };
 
 tablita();
 
 // CALCULAR COSTOS DE LA TABLA
-function calcularCosto () {
-let sum = [];
-let inRef = document.getElementsByClassName("nilai");
-for(var i=0; i<inRef.length; i++){
-  if(inRef[i].value != 0 ) {
-    sum.push(parseFloat(inRef[i].value));
-
+function calcularCosto() {
+  let sum = [];
+  let inRef = document.getElementsByClassName("nilai");
+  for (var i = 0; i < inRef.length; i++) {
+    inRef[i].value != 0 && sum.push(parseFloat(inRef[i].value));
   }
+  const final = parseFloat(sum.reduce((acc, el) => acc + el, 0));
+
+  let costoRef = document.getElementById("displayCosto");
+  costoRef.innerHTML = `<p>Costo de la compra: $ ${final}</p>`;
 }
-const final = parseFloat(sum.reduce((acc, el) => acc + el, 0));
-
-let costoRef = document.getElementById("displayCosto");
-costoRef.innerHTML = `<p>Costo de la compra: $ ${final}</p>`
-};
-
-
-
