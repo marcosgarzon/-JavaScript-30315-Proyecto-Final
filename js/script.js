389326c115102fd2fbe9737e5a5ba5422c9b4d71
Marcos Garzon <marcosgarzonmdq@gmail.com>
2022-04-17 15:53:24 -0300

    final

--- js/script.js
+++ js/script.js
@@ -1,28 +1,21 @@
-// AUTOEJECUTABLE, FOTO QUE CAMBIA SEGÚN EL DÍA DE LA SEMANA
+// AUTOEJECUTABLE, CAMBIA SEGÚN EL DÍA DE LA SEMANA
 (() => {
   let tiempo = new Date();
   let semana = tiempo.getDay();
   if (semana == 0) {
-    document.getElementById("ofert").innerHTML =
-      "<p> Domingo </p>";
+    document.getElementById("ofert").innerHTML = "<p> Domingo </p>";
   } else if (semana == 1) {
-    document.getElementById("ofert").innerHTML =
-    "<p> Lunes </p>";
+    document.getElementById("ofert").innerHTML = "<p> Lunes </p>";
   } else if (semana == 2) {
-    document.getElementById("ofert").innerHTML =
-    "<p> Martes </p>";
+    document.getElementById("ofert").innerHTML = "<p> Martes </p>";
   } else if (semana == 3) {
-    document.getElementById("ofert").innerHTML =
-    "<p> Miércoles </p>";
+    document.getElementById("ofert").innerHTML = "<p> Miércoles </p>";
   } else if (semana == 4) {
-    document.getElementById("ofert").innerHTML =
-    "<p> Jueves </p>";
+    document.getElementById("ofert").innerHTML = "<p> Jueves </p>";
   } else if (semana == 5) {
-    document.getElementById("ofert").innerHTML =
-    "<p> Viernes </p>";
+    document.getElementById("ofert").innerHTML = "<p> Viernes </p>";
   } else {
-    document.getElementById("ofert").innerHTML =
-    "<p> Sábado </p>";
+    document.getElementById("ofert").innerHTML = "<p> Sábado </p>";
   }
 })();
 
@@ -35,33 +28,30 @@ async function mostrarClima() {
   let ciudadValor = document.getElementById("ciudad-valor");
   let climaValor = document.getElementById("clima-valor");
 
-  navigator.geolocation && navigator.geolocation.getCurrentPosition ( ubicacion => { 
-    lon = ubicacion.coords.longitude
-    lat = ubicacion.coords.latitude
+  navigator.geolocation &&
+    navigator.geolocation.getCurrentPosition((ubicacion) => {
+      lon = ubicacion.coords.longitude;
+      lat = ubicacion.coords.latitude;
 
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&lang=es&units=metric&appid=717511e03d5cd7a357581c1b611cd713`;
-    http://openweathermap.org/img/wn/10d@2x.png
-    console.log(url)
+      const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&lang=es&units=metric&appid=717511e03d5cd7a357581c1b611cd713`;
+      //openweathermap.org/img/wn/10d@2x.png
+      http: console.log(url);
 
-    fetch(url)
-    .then( respuesta => respuesta.json() )
-    .then( datos => {
-         console.log(datos);
-         let temp = Math.round(datos.main.temp);
-         let ciudad = datos.name;
-         let clima = datos.weather[0].description;
-         let icono = datos.weather[0].icon;
-         const urlIcon = `http://openweathermap.org/img/wn/${icono}.png`
-         
-         temperaturaValor.textContent = `${temp}º C`;
-         ciudadValor.textContent = `${ciudad}`;
-         climaValor.innerHTML = `<p>${clima}</p> <img src="${urlIcon}">`;
-    })
+      fetch(url)
+        .then((respuesta) => respuesta.json())
+        .then((datos) => {
+          let temp = Math.round(datos.main.temp);
+          let ciudad = datos.name;
+          let clima = datos.weather[0].description;
+          let icono = datos.weather[0].icon;
+          const urlIcon = `http://openweathermap.org/img/wn/${icono}.png`;
 
-  });
- 
-
-};
+          temperaturaValor.textContent = `${temp}º C`;
+          ciudadValor.textContent = `${ciudad}`;
+          climaValor.innerHTML = `<p>${clima}</p> <img src="${urlIcon}">`;
+        });
+    });
+}
 
 mostrarClima();
 
@@ -104,9 +94,9 @@ function mostrarElementos() {
   contenedor.innerHTML = "";
 
   for (const element of elementos) {
-    const item = document.createElement("li");        
-    item.className = "item-lista text-center";       
-    item.addEventListener("click", (e) => {           
+    const item = document.createElement("li");
+    item.className = "item-lista text-center";
+    item.addEventListener("click", (e) => {
       e.target.remove();
       const index = elementos.findIndex((el) => el === e.target.textContent);
       elementos.splice(index, 1);
@@ -152,4 +142,4 @@ function calcularCosto() {
 
   let costoRef = document.getElementById("displayCosto");
   costoRef.innerHTML = `<p>Costo de la compra: $ ${final}</p>`;
-}
+};
